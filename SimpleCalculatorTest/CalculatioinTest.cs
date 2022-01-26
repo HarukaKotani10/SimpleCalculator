0ebc7cb2f0ff5c9425f4e36be6f54066f974ce10
Haruka Kotani <harukakotani28@gmail.com>
2022-01-25 22:05:57 -0800

    added a test project

---
+++ SimpleCalculatorTest/CalculatioinTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SimpleCalculatorTest
+{
+    public class CalculatioinTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
